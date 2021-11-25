e82d61b6eb4e52e7a37d8d31eeca30d90ea9bc26
Roman Volozhanin <rsvolozhanin@gmail.com>
2021-11-25 15:46:53 +0300

    remove city choosing

--- storage_choosing.py
+++ storage_choosing.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
 from telegram.ext import (CallbackContext, CommandHandler, ConversationHandler,
                           Filters, MessageHandler, Updater)
@@ -7,42 +9,30 @@ from load import logger, DATABASE_URL, keyboard_row_divider
 
 
 class StateEnum(Enum):
-    CITY = auto()
+    START = auto()
     STORAGE = auto()
 
 
-def start_handler(update: Update, context: CallbackContext):
-    """Start conversation"""
-    logger.info(f'User {update.effective_user["id"]} started bot')
-    reply_keyboard = [["Москва"]]
-    update.message.reply_text(
-        "Привет! Я помогу тебе подобрать, забронировать и "
-        "арендовать пространство для твоих вещей\nВыберите, "
-        "пожалуйста, город:",
-        reply_markup=ReplyKeyboardMarkup(
-            reply_keyboard,
-            one_time_keyboard=True,
-            input_field_placeholder="Ваш город?",
-            resize_keyboard=True,
-        ),
-    )
-    return StateEnum.CITY
+def get_storages() -> List[str]:
+    """Get list of storages from DB"""
+    # connection = psycopg2.connect(DATABASE_URL)
+    # cursor = connection.cursor()
+    # cursor.execute("SELECT Address FROM storages")
+    # storages_in_db = cursor.fetchall()
+    # storages = []
+    # for storage in storages_in_db:
+    #     storages.append(''.join(storage))
+    storages = ['test1', 'test2', 'test3', 'test4']
+    # cursor.close()
+    return storages
 
 
-def city(update: Update, context: CallbackContext):
-    """Handle city name"""
-    connection = psycopg2.connect(DATABASE_URL)
-    cursor = connection.cursor()
-    cursor.execute("SELECT Address FROM storages")
-    storages_in_db = cursor.fetchall()
-    storages = []
-    for storage in storages_in_db:
-        storages.append(''.join(storage))
-    city_name = update.message.text
+def storage_question(update: Update, context: CallbackContext) -> StateEnum:
+    storages = get_storages()
 
     reply_keyboard = list(keyboard_row_divider(storages))
     update.message.reply_text(
-        f"Выберете адрес хранилища в городе {city_name}:",
+        f"Выберете адрес хранилища:",
         reply_markup=ReplyKeyboardMarkup(
             reply_keyboard,
             one_time_keyboard=True,
@@ -50,12 +40,25 @@ def city(update: Update, context: CallbackContext):
             resize_keyboard=True,
         ),
     )
-    # cursor.close()
     return StateEnum.STORAGE
 
 
-def storage(update: Update, context: CallbackContext):
+def start_handler(update: Update, context: CallbackContext) -> StateEnum:
+    """Start conversation"""
+    logger.info(f'User {update.effective_user["id"]} started bot')
+    update.message.reply_text(
+        "Привет! Я помогу тебе подобрать, забронировать и "
+        "арендовать пространство для твоих вещей")
+    return storage_question(update, context)
+
+
+def storage(update: Update, context: CallbackContext) -> StateEnum:
     """Handle storage address"""
+    storage_name = update.message.text
+    if storage_name not in get_storages():
+        update.message.reply_text(f"Простите, по данному адресу у нас пока нет складов (")
+        return storage_question(update, context)
+
     update.message.reply_text(
         f"Вы выбрали хранилище по адресу: {update.message.text}",
         reply_markup=ReplyKeyboardRemove()
@@ -77,10 +80,7 @@ def get_choosing_handler():
     return ConversationHandler(
         entry_points=[CommandHandler("start", start_handler)],
         states={
-            StateEnum.CITY: [
-                MessageHandler(
-                    Filters.regex("^(Москва)$"),
-                    city)],
+            StateEnum.START: [MessageHandler(Filters.text, start_handler)],
             StateEnum.STORAGE: [MessageHandler(Filters.text, storage)],
         },
         fallbacks=[CommandHandler("cancel", cancel)],
