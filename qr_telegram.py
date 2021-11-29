43d78e79f37eb2656369a268165df4b1cd4eeaee
b10t <b10t.rus@gmail.com>
2021-11-29 22:39:58 +0800

    Указание кодировки при формировании QR.

--- qr_telegram.py
+++ qr_telegram.py
@@ -1,16 +1,17 @@
-from pyqrcode import QRCode
 import os
+from builtins import getattr
+
 import png
-
+from pyqrcode import QRCode
 from telegram import (ForceReply, InlineKeyboardButton, InlineKeyboardMarkup,
-                      ParseMode, ReplyKeyboardMarkup, ReplyKeyboardRemove,
-                      Update, KeyboardButton)
-from telegram.ext import (CallbackContext, CallbackQueryHandler,
-                          CommandHandler, ConversationHandler, Filters,
-                          PreCheckoutQueryHandler, MessageHandler)
+                      KeyboardButton, ParseMode, ReplyKeyboardMarkup,
+                      ReplyKeyboardRemove, Update)
+from telegram.ext import (CallbackContext, CallbackQueryHandler, CommandHandler,
+                          ConversationHandler, Filters, MessageHandler,
+                          PreCheckoutQueryHandler)
 
 
-def get_qr_code(update: Update, context: CallbackContext) -> None:
+def get_qr_code(update: Update, context: CallbackContext) -> int:
     """get qr code for text_in_code"""
     text_in_code = [
         context.user_data['last_name'],
@@ -20,14 +21,16 @@ def get_qr_code(update: Update, context: CallbackContext) -> None:
         str(context.user_data['invoice_price'])
     ]
     message_id = update.message.message_id
-    text = ' '.join(str(text_in_code)
-    qr = QRCode(text)
+    text = ''.join(str(text_in_code))
+    qr = QRCode(text, encoding='utf8')
     qr.png('code.png', scale=10)
     update.message.reply_photo(
         photo=open('code.png', 'rb'),
         reply_to_message_id=message_id,
         caption='Ваш QR-код для входа в хранилище')
     os.remove('code.png')
+
+    return ConversationHandler.END
 
 
 def add_qr_handlers(dispatcher) -> None:
